263c35c9622e79bcfc85f53fbb0ad529f6e156ba
Chanuka Samila <chanuka.samila@outlook.com>
2024-01-10 12:43:54 +0530

    Added environment files.

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class ProductService {
-    url: string = "https://localhost:7263";
+    url: string = environment.apiBaseUrl;
 
     constructor(private _http:HttpClient) {
     }
